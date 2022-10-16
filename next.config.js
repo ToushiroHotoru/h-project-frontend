c79fb651ff4e6c56127ba076195d5e1160f0bddd
ToushiroHotoru <toushirohotoru@gmail.com>
2022-10-16 11:26:46 +0300

    Тут не существенных изменений, мелочи

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ["i.pinimg.com", "images.hdqwalls.com"],
+		domains: ["i.pinimg.com", "images.hdqwalls.com", "get.wallhere.com"],
 	},
 };
 
