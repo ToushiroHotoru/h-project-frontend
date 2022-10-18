e8b0629577677c75ff58911164d6bbcc17c8dc02
ToushiroHotoru <toushirohotoru@gmail.com>
2022-10-18 12:18:37 +0300

    Стилизовал немного mangalist

--- components/Mangas/MangaList.js
+++ components/Mangas/MangaList.js
@@ -1,48 +1,62 @@
 import manga from "../../styles/components/MangaList.module.css";
 import Image from "next/image";
+import { Flex, Box, Tag, HStack, Heading, Spacer } from "@chakra-ui/react";
 
 export default function MangaList({ data }) {
   return (
-    <>
-      <div className={manga.list}>
-        <div className={manga.list_image_wrap}>
-          {/* {data &&
-            data.map((item) => {
-              return (
-                <Image
-                  className={manga.list_image}
-                  src="#"
-                  width={300}
-                  height={300}
-                  alt={item.title}
-                />
-              );
-            })} */}
+    <Flex className={manga.list} align="center">
+      <Flex h="250px" justify="center" align="center" px="20px">
+        <Box>
           <Image
-            // onLoad={onLoadHandler}
             src="https://i.pinimg.com/originals/75/2d/21/752d2198f907d328b1403f91b2a7ded0.jpg"
-            layout="intrinsic"
             alt="pic"
             width={200}
-            height={300}
+            height={200}
           />
-        </div>
-        <div className={manga.list_info}>
-          <div className={manga.list_title}>{data.title}</div>
-          <div className={manga.list_series}>{data.series}</div>
-          <div className={manga.list_artist}>{data.artist}</div>
-          <div className={manga.list_tags}>
+        </Box>
+        <Box>
+          <Image
+            src="https://i.pinimg.com/originals/75/2d/21/752d2198f907d328b1403f91b2a7ded0.jpg"
+            alt="pic"
+            width={200}
+            height={200}
+          />
+        </Box>
+      </Flex>
+
+      <Flex direction="column" h="200px">
+        <Flex>
+          <Box fontSize="18px" w="100px">
+            Название
+          </Box>
+          <Box fontSize="18px" w="100px">
+            {data.title}
+          </Box>
+        </Flex>
+        <Flex>
+          <Box w="80px">Серия</Box>
+          <Box w="80px">{data.series}</Box>
+        </Flex>
+        <Flex>
+          <Box w="80px">Автор</Box>
+          <Box w="80px">{data.artist}</Box>
+        </Flex>
+
+        <Flex>
+          <Box minW="80px" verticalAlign="center">
+            Теги:
+          </Box>
+          <HStack w="100%">
             {data.tags.map((tag, i) => {
               return (
-                <div className={manga.list_tag} key={i + 1}>
+                <Tag key={i + 1} variant="solid" colorScheme="dark ">
                   {tag}
-                </div>
+                </Tag>
               );
             })}
-          </div>
-          <div className={manga.list_info}></div>
-        </div>
-      </div>
-    </>
+          </HStack>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
