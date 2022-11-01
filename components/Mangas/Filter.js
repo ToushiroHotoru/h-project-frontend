dbb1e1ec53247073da4b95a22b6b0d62eaea46bc
ToushiroHotoru <toushirohotoru@gmail.com>
2022-11-01 15:42:50 +0300

    Сделал сортировку по последним добавленным и по алфавиту

--- components/Mangas/Filter.js
+++ components/Mangas/Filter.js
@@ -6,29 +6,102 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  useDisclosure,
+  Button,
+  Flex,
+  Box,
+  Tooltip,
+  Center,
 } from "@chakra-ui/react";
-import { useDisclosure, Select, Box, Button, Lorem } from "@chakra-ui/react";
+import {
+  BsFillClockFill,
+  BsSortAlphaDown,
+  BsFillStarFill,
+  BsFillHeartFill,
+  BsFillEyeFill,
+} from "react-icons/bs";
+import css from "../../styles/components/Filter.module.css";
 
-export default function Filter() {
+export default function Filter({ sortType, setSortType }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Button onClick={onOpen}>Расширенный поиск</Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} size="xs">
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Расширенный поиск</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <>
-              <Box>
-                <Select placeholder="Select option">
-                  <option value="option1">Option 1</option>
-                  <option value="option2">Option 2</option>
-                  <option value="option3">Option 3</option>
-                </Select>
-              </Box>
-            </>
+          <ModalBody display="flex" justifyContent="center">
+            <Box w="100%">
+              <Box>Сортировка</Box>
+              <Flex w="100%" mt="10px">
+                <Tooltip label="Дата" hasArrow>
+                  <div
+                    className={`${css.sort_item} ${
+                      sortType === "latest" && css.sort_item_active
+                    }`}
+                    onClick={() => {
+                      setSortType("latest");
+                    }}
+                  >
+                    <BsFillClockFill size="50%" />
+                  </div>
+                </Tooltip>
+
+                <Tooltip label="Алфавит" hasArrow>
+                  <div
+                    className={`${css.sort_item} ${
+                      sortType === "alphabet" && css.sort_item_active
+                    }`}
+                    onClick={() => {
+                      setSortType("alphabet");
+                    }}
+                  >
+                    <BsSortAlphaDown size="50%" />
+                  </div>
+                </Tooltip>
+
+                <Tooltip label="Рейтинг" hasArrow>
+                  <div
+                    className={`${css.sort_item} ${
+                      sortType === "rating" && css.sort_item_active
+                    }`}
+                    onClick={() => {
+                      setSortType("rating");
+                    }}
+                  >
+                    <BsFillStarFill size="50%" />
+                  </div>
+                </Tooltip>
+
+                <Tooltip label="Нравится" hasArrow>
+                  <div
+                    className={`${css.sort_item} ${
+                      sortType === "like" && css.sort_item_active
+                    }`}
+                    onClick={() => {
+                      setSortType("like");
+                    }}
+                  >
+                    <BsFillHeartFill size="50%" />
+                  </div>
+                </Tooltip>
+
+                <Tooltip label="Просмотры" hasArrow>
+                  <Box
+                    className={`${css.sort_item} ${
+                      sortType === "view" && css.sort_item_active
+                    }`}
+                    onClick={() => {
+                      setSortType("view");
+                    }}
+                  >
+                    <BsFillEyeFill size="50%" />
+                  </Box>
+                </Tooltip>
+              </Flex>
+            </Box>
           </ModalBody>
 
           <ModalFooter d="flex" justifyContent="center">
