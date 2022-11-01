f92041d0b81db72ad6ac810efbb9626d1cf61e27
ToushiroHotoru <toushirohotoru@gmail.com>
2022-11-01 15:40:27 +0300

    Убрал старый код для theme, который не работал

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
-
-const config = {
-  initialColorMode: "#000",
-  useSystemColorMode: false,
-};
-
-const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider initialColorMode={theme.config.initialColorMode}>
+    <ChakraProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
