9847ef2155323b8594b3bdf0e2bd27fc6669010f
ToushiroHotoru <toushirohotoru@gmail.com>
2022-11-03 15:12:00 +0300

    Небольшая оптимизация верстки

--- components/Mangas/MangaList.js
+++ components/Mangas/MangaList.js
@@ -4,43 +4,40 @@ import { Flex, Box, Tag, HStack, Skeleton } from "@chakra-ui/react";
 import { useState } from "react";
 
 export default function MangaList({ data }) {
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [galleryExtended, setGalleryExtended] = useState(false);
-	const onLoadHandler = () => {
-		setIsLoaded(true);
-	};
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [galleryExtended, setGalleryExtended] = useState(false);
+  const onLoadHandler = () => {
+    setIsLoaded(true);
+  };
 
-	return (
-		<Skeleton isLoaded={isLoaded}>
-			<Flex
-				className={manga.list}
-				align='center'>
-				<Flex
-					h='250px'
-					justify='center'
-					align='center'
-					className={manga.image_wrap}>
-					onMouseEnter=
-					{() => {
-						setGalleryExtended(true);
-					}}
-					{[4, 5, 6].map((_, i) => {
-						return (
-							<Box
-								key={i + 1}
-								className={`${manga.image}`}>
-								<Image
-									onLoad={onLoadHandler}
-									src={`/manga_cover/cover_${_}.jpg`}
-									alt='pic'
-									layout='intrinsic'
-									width={160}
-									height={200}
-								/>
-							</Box>
-						);
-					})}
-					{/* <Box>
+  return (
+    <Skeleton isLoaded={isLoaded}>
+      <Flex className={manga.list} align="center">
+        <Flex
+          h="250px"
+          justify="center"
+          align="center"
+          className={manga.image_wrap}
+        >
+          {/* onMouseEnter= */}
+          {() => {
+            setGalleryExtended(true);
+          }}
+          {[4, 5, 6].map((_, i) => {
+            return (
+              <Box key={i + 1} className={`${manga.image}`}>
+                <Image
+                  onLoad={onLoadHandler}
+                  src={`/manga_cover/cover_${_}.jpg`}
+                  alt="pic"
+                  layout="intrinsic"
+                  width={160}
+                  height={200}
+                />
+              </Box>
+            );
+          })}
+          {/* <Box>
 						<Image
 							onLoad={onLoadHandler}
 							src='/manga_cover/cover_6.jpg'
@@ -50,54 +47,42 @@ export default function MangaList({ data }) {
 							height={200}
 						/>
 					</Box> */}
-				</Flex>
+        </Flex>
 
-				<Flex
-					direction='column'
-					h='200px'
-					className={manga.info}>
-					<Flex w='100%'>
-						<Box
-							fontSize='18px'
-							w='100px'>
-							Название
-						</Box>
-						<Box
-							fontSize='18px'
-							w='100px'>
-							{data.title}
-						</Box>
-					</Flex>
-					<Flex>
-						<Box w='100px'>Серия</Box>
-						<Box w='80px'>{data.series}</Box>
-					</Flex>
-					<Flex>
-						<Box w='100px'>Автор</Box>
-						<Box w='80px'>{data.artist}</Box>
-					</Flex>
+        <Flex direction="column" h="200px" className={manga.info}>
+          <Flex w="100%">
+            <Box fontSize="18px" w="100px">
+              Название
+            </Box>
+            <Box fontSize="18px" w="100px">
+              {data.title}
+            </Box>
+          </Flex>
+          <Flex>
+            <Box w="100px">Серия</Box>
+            <Box w="80px">{data.series}</Box>
+          </Flex>
+          <Flex>
+            <Box w="100px">Автор</Box>
+            <Box w="80px">{data.artist}</Box>
+          </Flex>
 
-					<Flex>
-						<Box
-							minW='100px'
-							verticalAlign='center'>
-							Теги:
-						</Box>
-						<HStack w='100%'>
-							{data.tags.map((tag, i) => {
-								return (
-									<Tag
-										key={i + 1}
-										variant='solid'
-										colorScheme='dark '>
-										{tag}
-									</Tag>
-								);
-							})}
-						</HStack>
-					</Flex>
-				</Flex>
-			</Flex>
-		</Skeleton>
-	);
+          <Flex>
+            <Box minW="100px" verticalAlign="center">
+              Теги:
+            </Box>
+            <HStack w="100%">
+              {data.tags.map((tag, i) => {
+                return (
+                  <Tag key={i + 1} variant="solid" colorScheme="dark ">
+                    {tag}
+                  </Tag>
+                );
+              })}
+            </HStack>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Skeleton>
+  );
 }
