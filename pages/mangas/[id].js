fbc3a9b49d35a7281dcf86af34dc9f2da357cd65
Kamiya_Nya <dikriss16@gmail.com>
2022-11-11 14:55:47 +0300

    Добавил дату

--- pages/mangas/[id].js
+++ pages/mangas/[id].js
@@ -1,13 +1,15 @@
 import { getPaths } from "../../libs/get_post.js";
 import css from "../../styles/pages/Manga.module.css";
 import { useEffect, useState } from "react";
-import { Box, Center, Divider, Heading } from "@chakra-ui/react";
+import { Box, Center, Divider, Flex, Heading } from "@chakra-ui/react";
 import Comments from "../../components/Manga/Comments.js";
 import Pages from "../../components/Manga/Pages.js";
 import HeadTags from "../../components/Manga/HeadTags.js";
 import HeadDesc from "../../components/Manga/HeadDesc.js";
 import HeadImg from "../../components/Manga/HeadImg.js";
 import Head from "next/head";
+import moment from "moment";
+import "moment/locale/ru";
 
 export async function getStaticProps({ params }) {
 	const { id } = params;
@@ -38,6 +40,7 @@ export async function getStaticPaths(context) {
 }
 
 export default function Manga({ manga, id }) {
+	moment.locale("ru");
 	const [mangaDynamic, setMangaDynamic] = useState();
 
 	const onLoadHander = async () => {
@@ -68,20 +71,24 @@ export default function Manga({ manga, id }) {
 			</Head>
 			<div className={css.wrap}>
 				<div className='container'>
-					<Heading
-						as='h1'
-						size='xl'
-						className={css.title}>
-						Manga - {manga.title}
-					</Heading>
-					<div className={css.head}>
+					<Flex justifyContent='space-between'>
+						<Heading
+							as='h1'
+							size='md'
+							className={css.title}>
+							Manga - {manga.title}
+						</Heading>
+						<Box mt='auto'>{moment(manga.updatedAt).format("DD MMMM YYYY")}</Box>
+					</Flex>
+
+					<section className={css.head}>
 						<HeadImg />
 						<HeadDesc
 							mangaDynamic={mangaDynamic}
 							manga={manga}
 						/>
 						<HeadTags tags={mangaDynamic && mangaDynamic.tags} />
-					</div>
+					</section>
 					<Pages
 						pages={mangaDynamic && mangaDynamic.pages}
 						manga={manga}
