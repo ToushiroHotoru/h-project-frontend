d9a79c1b17615142b63f48180dcc99c678861e17
Kamiya_Nya <dikriss16@gmail.com>
2022-11-11 11:01:52 +0300

    HStack заменил на flex

--- components/Manga/HeadTags.js
+++ components/Manga/HeadTags.js
@@ -1,37 +1,41 @@
 import css from "../../styles/components/MangaHead.module.css";
-import { Box, Center, HStack } from "@chakra-ui/react";
+import { Box, Center, HStack, Flex } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function HeadTags({ tags }) {
-  return (
-    <div className={css.head_tags}>
-      <HStack alignItems="flex-start">
-        {tags &&
-          tags.map((item, i) => {
-            return (
-              <Box
-                borderRadius="5px"
-                border="1px solid #fff"
-                maxHeight="100px"
-                minWidth="80px"
-                overflow="hidden"
-                key={i + 1}
-              >
-                <Box>
-                  <Image
-                    src="/tristana.png"
-                    alt="Picture of the author"
-                    layout="responsive"
-                    objectFit="cover"
-                    width={250}
-                    height={200}
-                  />
-                </Box>
-                <Center>{item}</Center>
-              </Box>
-            );
-          })}
-      </HStack>
-    </div>
-  );
+	return (
+		<div className={css.head_tags}>
+			<Flex
+				alignItems='flex-start'
+				flexWrap='wrap'
+				spacing='5px'>
+				{tags &&
+					tags.map((item, i) => {
+						return (
+							<Box
+								borderRadius='5px'
+								border='1px solid #fff'
+								maxHeight='100px'
+								minWidth='80px'
+								overflow='hidden'
+								marginInline={0}
+								margin='0 5px 5px 0'
+								key={i + 1}>
+								<Box>
+									<Image
+										src='/tristana.png'
+										alt='Picture of the author'
+										layout='responsive'
+										objectFit='cover'
+										width={250}
+										height={200}
+									/>
+								</Box>
+								<Center>{item}</Center>
+							</Box>
+						);
+					})}
+			</Flex>
+		</div>
+	);
 }
