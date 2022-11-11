b1a28e2f61ac737ec3730eb9365db130511e238f
Kamiya_Nya <dikriss16@gmail.com>
2022-11-11 14:53:37 +0300

    Добавил переход на мангу и поработал немного над структурой

--- components/Mangas/MangaList.js
+++ components/Mangas/MangaList.js
@@ -2,42 +2,44 @@ import manga from "../../styles/components/MangaList.module.css";
 import Image from "next/image";
 import { Flex, Box, Tag, HStack, Skeleton } from "@chakra-ui/react";
 import { useState } from "react";
-
+import Link from "next/link";
 export default function MangaList({ data }) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [galleryExtended, setGalleryExtended] = useState(false);
-  const onLoadHandler = () => {
-    setIsLoaded(true);
-  };
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [galleryExtended, setGalleryExtended] = useState(false);
+	const onLoadHandler = () => {
+		setIsLoaded(true);
+	};
 
-  return (
-    <Skeleton isLoaded={isLoaded}>
-      <Flex className={manga.list} align="center">
-        <Flex
-          h="250px"
-          justify="center"
-          align="center"
-          className={manga.image_wrap}
-        >
-          {/* onMouseEnter= */}
-          {() => {
-            setGalleryExtended(true);
-          }}
-          {Array.of(1,1,1).map((_, i) => {
-            return (
-              <Box key={i + 1} className={`${manga.image}`}>
-                <Image
-                  onLoad={onLoadHandler}
-                  src={`/manga_cover/cover_${_}.jpg`}
-                  alt="pic"
-                  layout="intrinsic"
-                  width={160}
-                  height={200}
-                />
-              </Box>
-            );
-          })}
-          {/* <Box>
+	return (
+		<Skeleton isLoaded={isLoaded}>
+			<Flex className={manga.list}>
+				<Link href={`/mangas/${data._id}`}>
+					<a>
+						<Flex
+							justify='center'
+							align='center'
+							className={manga.image_wrap}>
+							{/* onMouseEnter= */}
+							{() => {
+								setGalleryExtended(true);
+							}}
+							{Array.of(1, 1, 1).map((_, i) => {
+								return (
+									<Box
+										key={i + 1}
+										className={`${manga.image}`}>
+										<Image
+											onLoad={onLoadHandler}
+											src={`/manga_cover/cover_${_}.jpg`}
+											alt='pic'
+											layout='intrinsic'
+											width={160}
+											height={200}
+										/>
+									</Box>
+								);
+							})}
+							{/* <Box>
 						<Image
 							onLoad={onLoadHandler}
 							src='/manga_cover/cover_6.jpg'
@@ -47,42 +49,64 @@ export default function MangaList({ data }) {
 							height={200}
 						/>
 					</Box> */}
-        </Flex>
+						</Flex>
+					</a>
+				</Link>
 
-        <Flex direction="column" h="200px" className={manga.info}>
-          <Flex w="100%">
-            <Box fontSize="18px" w="100px">
-              Название
-            </Box>
-            <Box fontSize="18px" w="100px">
-              {data.title}
-            </Box>
-          </Flex>
-          <Flex>
-            <Box w="100px">Серия</Box>
-            <Box w="80px">{data.series}</Box>
-          </Flex>
-          <Flex>
-            <Box w="100px">Автор</Box>
-            <Box w="80px">{data.artist}</Box>
-          </Flex>
+				<Flex
+					direction='column'
+					className={manga.info}>
+					<Flex
+						w='100%'
+						mb='16px'>
+						<Box
+							fontSize='18px'
+							minW='100px'>
+							Название
+						</Box>
+						<Link href={`/mangas/${data._id}`}>
+							<a>
+								<Box
+									fontSize='18px'
+									w='100%'>
+									{data.title}
+								</Box>
+							</a>
+						</Link>
+					</Flex>
+					<Flex mb='8px'>
+						<Box minW='100px'>Серия</Box>
+						<Box w='100%'>{data.series}</Box>
+					</Flex>
+					<Flex mb='10px'>
+						<Box minW='100px'>Автор</Box>
+						<Box w='100%'>{data.artist}</Box>
+					</Flex>
 
-          <Flex>
-            <Box minW="100px" verticalAlign="center">
-              Теги:
-            </Box>
-            <HStack w="100%">
-              {data.tags.map((tag, i) => {
-                return (
-                  <Tag key={i + 1} variant="solid" colorScheme="dark ">
-                    {tag}
-                  </Tag>
-                );
-              })}
-            </HStack>
-          </Flex>
-        </Flex>
-      </Flex>
-    </Skeleton>
-  );
+					<Flex>
+						<Box
+							minW='100px'
+							verticalAlign='center'>
+							Теги:
+						</Box>
+						<Flex
+							w='100%'
+							flexWrap='wrap'>
+							{data.tags.map((tag, i) => {
+								return (
+									<Tag
+										key={i + 1}
+										variant='solid'
+										colorScheme='dark'
+										margin='0 3px 3px 0'>
+										{tag}
+									</Tag>
+								);
+							})}
+						</Flex>
+					</Flex>
+				</Flex>
+			</Flex>
+		</Skeleton>
+	);
 }
