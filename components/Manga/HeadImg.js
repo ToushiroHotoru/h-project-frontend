37e643f48612abaca94e70ed56bc4e166c662746
Kamiya_Nya <dikriss16@gmail.com>
2022-11-11 14:29:14 +0300

    Немного изменил структуру для иконок

--- components/Manga/HeadImg.js
+++ components/Manga/HeadImg.js
@@ -1,38 +1,61 @@
 import { Box, Center, HStack } from "@chakra-ui/react";
 import Image from "next/image";
-import {
-  BsDownload,
-  BsHeart,
-  BsHeartFill,
-  BsFillFolderFill,
-} from "react-icons/bs";
+import { BsDownload, BsHeart, BsHeartFill, BsFillFolderFill } from "react-icons/bs";
 
 import css from "../../styles/components/MangaHead.module.css";
 
 export default function HeadImg() {
-  return (
-    <div className={css.head_img}>
-      <Box borderRadius="8px" overflow="hidden">
-        <Image
-          src="/manga_cover/cover_1.jpg"
-          alt="Picture of the author"
-          layout="responsive"
-          width={350}
-          height={500}
-        />
-      </Box>
-      <HStack minHeight="40px" mt="15px" py="1em">
-        <Center>
-          {/* <Icon as={BsDownload} w={9} h={9} /> */}
-          <BsDownload size="20%" />
-        </Center>
-        <Center>
-          <BsHeart size="20%" />
-        </Center>
-        <Center>
-          <BsFillFolderFill size="20%" />
-        </Center>
-      </HStack>
-    </div>
-  );
+	return (
+		<div className={css.head_img}>
+			<Box
+				borderRadius='8px'
+				overflow='hidden'>
+				<Image
+					src='/manga_cover/cover_1.jpg'
+					alt='Picture of the author'
+					layout='responsive'
+					width={350}
+					height={500}
+				/>
+			</Box>
+			<HStack
+				minHeight='30px'
+				mt='15px'
+				justifyContent='space-between'>
+				<Box
+					maxWidth='40px'
+					width='100%'
+					padding='5px'>
+					<Center>
+						<BsDownload
+							size='90%'
+							cursor='pointer'
+						/>
+					</Center>
+				</Box>
+				<Box
+					maxWidth='40px'
+					width='100%'
+					padding='5px'>
+					<Center>
+						<BsHeart
+							size='90%'
+							cursor='pointer'
+						/>
+					</Center>
+				</Box>
+				<Box
+					maxWidth='40px'
+					width='100%'
+					padding='5px'>
+					<Center>
+						<BsFillFolderFill
+							size='90%'
+							cursor='pointer'
+						/>
+					</Center>
+				</Box>
+			</HStack>
+		</div>
+	);
 }
