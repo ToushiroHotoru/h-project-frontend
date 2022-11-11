0c375a1b45ad7bacc8f2d5395a287742a4e8a974
Kamiya_Nya <dikriss16@gmail.com>
2022-11-11 14:56:18 +0300

    Добавил title для страницы

--- pages/mangas/index.js
+++ pages/mangas/index.js
@@ -10,77 +10,88 @@ import { Box, HStack, Button } from "@chakra-ui/react";
 import Filter from "../../components/Mangas/Filter";
 import Pagination from "../../components/Mangas/Pagination";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Mangas() {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const router = useRouter();
-  const [isToggled, setIsToggled] = useState(false);
+	const router = useRouter();
+	const [isToggled, setIsToggled] = useState(false);
 
-  const { data, error } = useSWR(
-    `https://h-project.herokuapp.com/mangas?page=${router.query.page}&sort=${router.query.sort}`,
-    // `http://localhost:8080/mangas?page=${pageIndex}&sort=${sortType}`,
-    fetcher
-  );
+	const { data, error } = useSWR(
+		`https://h-project.herokuapp.com/mangas?page=${router.query.page}&sort=${router.query.sort}`,
+		// `http://localhost:8080/mangas?page=${pageIndex}&sort=${sortType}`,
+		fetcher
+	);
 
-  if (error)
-    return (
-      <Error>
-        <Image
-          src="/manga_cover/cover_error.png"
-          layout="intrinsic"
-          alt="error"
-          width={723}
-          height={693}
-        />
-      </Error>
-    );
+	if (error)
+		return (
+			<Error>
+				<Image
+					src='/manga_cover/cover_error.png'
+					layout='intrinsic'
+					alt='error'
+					width={723}
+					height={693}
+				/>
+			</Error>
+		);
 
-  // useEffect(() => {
-  //   if (!router.isReady) return;
-  // }, [router.isReady]);
+	// useEffect(() => {
+	//   if (!router.isReady) return;
+	// }, [router.isReady]);
 
-  return (
-    <div className={catalog.catalog}>
-      <div className="container">
-        <HStack w="100%" align="center" justify="right">
-          <Box>
-            <Filter router={router} />
-          </Box>
-          <Box>
-            <Toggler
-              isToggled={isToggled}
-              setIsToggled={(val) => {
-                setIsToggled(val);
-              }}
-            />
-          </Box>
-        </HStack>
+	return (
+		<>
+			<Head>
+				<title>Каталог манг</title>
+			</Head>
+			<div className={catalog.catalog}>
+				<div className='container'>
+					<HStack
+						w='100%'
+						align='center'
+						justify='right'>
+						<Box>
+							<Filter router={router} />
+						</Box>
+						<Box>
+							<Toggler
+								isToggled={isToggled}
+								setIsToggled={(val) => {
+									setIsToggled(val);
+								}}
+							/>
+						</Box>
+					</HStack>
 
-        <div
-          className={`${catalog.wrap} ${
-            isToggled ? catalog.grid_detail : catalog.grid_common
-          }`}
-        >
-          {data &&
-            data.mangas.map((item, i) => {
-              return isToggled ? (
-                <MangaList data={item} key={i + 1} />
-              ) : (
-                <MangaTile props={item} key={i + 1} />
-              );
-            })}
-        </div>
+					<div className={`${catalog.wrap} ${isToggled ? catalog.grid_detail : catalog.grid_common}`}>
+						{data &&
+							data.mangas.map((item, i) => {
+								return isToggled ? (
+									<MangaList
+										data={item}
+										key={i + 1}
+									/>
+								) : (
+									<MangaTile
+										props={item}
+										key={i + 1}
+									/>
+								);
+							})}
+					</div>
 
-        {data && (
-          <Pagination
-            router={router}
-            total={data.total}
-            offset={data.offset}
-            step={data.step}
-          />
-        )}
-      </div>
-    </div>
-  );
+					{data && (
+						<Pagination
+							router={router}
+							total={data.total}
+							offset={data.offset}
+							step={data.step}
+						/>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
