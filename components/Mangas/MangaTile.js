b8b0b99dd899ec6046bde3e5b0d4edc8dc094102
ToushiroHotoru <toushirohotoru@gmail.com>
2022-11-14 16:56:01 +0300

    Починил skeleton

--- components/Mangas/MangaTile.js
+++ components/Mangas/MangaTile.js
@@ -1,35 +1,37 @@
 import css from "../../styles/components/MangaTile.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { Box } from "@chakra-ui/react";
 import { useState } from "react";
-import { Skeleton, SkeletonText } from "@chakra-ui/react";
 
 export default function MangaTile({ props }) {
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
   const onLoadHandler = () => {
-    setIsLoaded(true);
+    // событие после загрузки изображения
   };
 
   return (
     <Link href={`/mangas/${props._id}`}>
-      <a className={css.manga_tile}>
-        <Skeleton isLoaded={isLoaded}>
-          <Image
-            onLoad={onLoadHandler}
-            src={
-              props.cover === "Cover"
-                ? "/manga_cover/cover_1.jpg"
-                : "/manga_cover/cover_2.jpg"
-            }
-            layout="responsive"
-            alt="pic"
-            width={500}
-            height={700}
-          />
-        </Skeleton>
-        <SkeletonText isLoaded={isLoaded} mt="4">
+      <a
+        className={css.manga_tile}
+        onMouseOver={() => {
+          setIsMouseOver(true);
+          console.log("mouse is over ");
+        }}
+      >
+        <Image
+          onLoad={onLoadHandler}
+          src={props.cover}
+          layout="responsive"
+          alt="pic"
+          width={500}
+          height={700}
+        />
+
+        <Box mt="4">
           <div className={css.manga_tile_title}>{props.title}</div>
-        </SkeletonText>
+        </Box>
       </a>
     </Link>
   );
