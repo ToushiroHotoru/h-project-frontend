b154bc442cbebdf349b3729e1556f46a7cc2e62b
Kamiya_Nya <dikriss16@gmail.com>
2022-11-28 11:36:36 +0300

    Название манги перенес на картинку

--- components/Mangas/MangaTile.js
+++ components/Mangas/MangaTile.js
@@ -1,46 +1,63 @@
-import css from "../../styles/components/MangaTile.module.css";
-import Image from "next/image";
 import Link from "next/link";
-import { Box } from "@chakra-ui/react";
+import Image from "next/image";
 import { useState } from "react";
+import { Flex, Box } from "@chakra-ui/react";
+
+import css from "../../styles/components/MangaTile.module.css";
 
 export default function MangaTile({ props }) {
-  const [isMouseOver, setIsMouseOver] = useState(false);
+	const [isMouseOver, setIsMouseOver] = useState(false);
 
-  const onLoadHandler = () => {
-    // событие после загрузки изображения
-  };
+	const onLoadHandler = () => {
+		// событие после загрузки изображения
+	};
 
-  // const cutLongTitle = (title) => {
-  //   if (title.length > 88) {
-  //     console.log(title.slice(0, 85));
-  //     return title.slice(0, 100);
-  //   }
-  //   return title;
-  // };
+	// const cutLongTitle = (title) => {
+	//   if (title.length > 88) {
+	//     console.log(title.slice(0, 85));
+	//     return title.slice(0, 100);
+	//   }
+	//   return title;
+	// };
 
-  return (
-    <Link href={`/mangas/${props._id}`}>
-      <a
-        className={css.manga_tile}
-        onMouseOver={() => {
-          setIsMouseOver(true);
-          console.log("mouse is over ");
-        }}
-      >
-        <Image
-          onLoad={onLoadHandler}
-          src={props.cover}
-          layout="responsive"
-          alt="pic"
-          width={500}
-          height={700}
-        />
+	return (
+		<Link href={`/mangas/${props._id}`}>
+			<a
+				className={css.manga_tile}
+				onMouseOver={() => {
+					setIsMouseOver(true);
+				}}>
+				<Image
+					onLoad={onLoadHandler}
+					src={props.cover}
+					layout='responsive'
+					alt='pic'
+					width={500}
+					height={700}
+				/>
 
-        <Box mt="4" maxHeight="50px" overflowY="hidden" height="50px">
-          <div className={css.manga_tile_title}>{props.title}</div>
-        </Box>
-      </a>
-    </Link>
-  );
+				<Flex
+					position='absolute'
+					left='0'
+					bottom='0'
+					zIndex='2'
+					width='100%'
+					px={{ base: "4px", sm: "8px" }}
+					pb={{ base: "4px", sm: "6px" }}
+					height={{ base: "50%", sm: "30%" }}
+					bgGradient='linear(to-t, rgba(0,0,0,0.8), rgba(0,0,0,0))'>
+					<Box
+						mt='auto'
+						maxHeight={{ base: "34px", sm: "48px" }}
+						fontSize={{ base: "14px", sm: "18px" }}
+						lineHeight='125%'
+						overflowY='hidden'
+            className={css.m_tile_title}
+            >
+						{props.title}
+					</Box>
+				</Flex>
+			</a>
+		</Link>
+	);
 }
