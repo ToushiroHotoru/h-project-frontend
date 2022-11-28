511d8bb5c4f99f35e3232f3a5ebbc42d671dbe12
Kamiya_Nya <dikriss16@gmail.com>
2022-11-28 11:56:11 +0300

    Везде поменял ссылку

--- pages/mangas/index.js
+++ pages/mangas/index.js
@@ -1,121 +1,129 @@
-import catalog from "../../styles/pages/Catalog.module.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Skeleton, Flex, Box, HStack, Center } from "@chakra-ui/react";
+import { useState } from "react";
+import Image from "next/image";
 import useSWR from "swr";
+
+import catalog from "../../styles/pages/Catalog.module.css";
 import MangaTile from "../../components/Mangas/MangaTile";
 import MangaList from "../../components/Mangas/MangaList";
 import ErrorWrapper from "../../components/partials/ErrorWrapper";
-import Image from "next/image";
 import Toggler from "../../components/Mangas/Toggler";
-import { useState } from "react";
 import Filter from "../../components/Mangas/Filter";
 import Pagination from "../../components/Mangas/Pagination";
-import { useRouter } from "next/router";
-import Head from "next/head";
-import { Skeleton, Flex, Box, HStack, Center } from "@chakra-ui/react";
+import { LINK } from "../../libs/changeApiUrl.js";
 
 export default function Mangas() {
-  //   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	//   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	const router = useRouter();
+	const [isToggled, setIsToggled] = useState(false);
 
-  const router = useRouter();
-  const [isToggled, setIsToggled] = useState(false);
+	//   useEffect(() => {
+	//     if (!router.isReady) return;
+	//   }, [router.isReady]);
 
-  //   useEffect(() => {
-  //     if (!router.isReady) return;
-  //   }, [router.isReady]);
+	const fetcher = async (url) => {
+		if (!router.isReady) return;
+		const res = await fetch(url);
 
-  const fetcher = async (url) => {
-    if (!router.isReady) return;
-    const res = await fetch(url);
+		if (!res.ok) {
+			const error = new Error("An error occurred while fetching the data.");
+			error.info = await res.json();
+			throw error;
+		}
 
-    if (!res.ok) {
-      const error = new Error("An error occurred while fetching the data.");
-      error.info = await res.json();
-      throw error;
-    }
+		return res.json();
+	};
 
-    return res.json();
-  };
+	const { data, error } = useSWR(`${LINK}/mangas?page=${router.query.page}&sort=${router.query.sort}`, fetcher);
 
-  const { data, error } = useSWR(
-    `https://h-project.herokuapp.com/mangas?page=${router.query.page}&sort=${router.query.sort}`,
-    // `http://localhost:8080/mangas?page=${router.query.page}&sort=${router.query.sort}`,
-    fetcher
-  );
+	if (error) {
+		return (
+			<ErrorWrapper
+				link='/mangas?page=1&sort=latest'
+				linkTitle='Перейти на существующую страницу'>
+				<Image
+					src='/manga_cover/cover_error.png'
+					layout='intrinsic'
+					alt='error'
+					width={723}
+					height={693}
+				/>
+				<Center
+					color='red'
+					fontSize='24px'>
+					Error: {error.info.message}
+				</Center>
+			</ErrorWrapper>
+		);
+	}
 
-  if (error) {
-    return (
-      <ErrorWrapper
-        link="/mangas?page=1&sort=latest"
-        linkTitle="Перейти на существующую страницу"
-      >
-        <Image
-          src="/manga_cover/cover_error.png"
-          layout="intrinsic"
-          alt="error"
-          width={723}
-          height={693}
-        />
-        <Center color="red" fontSize="24px">
-          Error: {error.info.message}
-        </Center>
-      </ErrorWrapper>
-    );
-  }
+	return (
+		<>
+			<Head>
+				<title>Каталог</title>
+			</Head>
+			<div className={catalog.catalog}>
+				<div className='container'>
+					<HStack
+						w='100%'
+						align='center'
+						justify='right'>
+						<Box>
+							<Filter router={router} />
+						</Box>
+						<Box>
+							<Toggler
+								isToggled={isToggled}
+								setIsToggled={(val) => {
+									setIsToggled(val);
+								}}
+							/>
+						</Box>
+					</HStack>
 
-  return (
-    <>
-      <Head>
-        <title>Каталог</title>
-      </Head>
-      <div className={catalog.catalog}>
-        <div className="container">
-          <HStack w="100%" align="center" justify="right">
-            <Box>
-              <Filter router={router} />
-            </Box>
-            <Box>
-              <Toggler
-                isToggled={isToggled}
-                setIsToggled={(val) => {
-                  setIsToggled(val);
-                }}
-              />
-            </Box>
-          </HStack>
+					<div className={`${catalog.wrap} ${isToggled ? catalog.grid_detail : catalog.grid_common}`}>
+						{!data &&
+							[...Array(24)].map((_, i) => {
+								return (
+									<Flex
+										flexDirection='column'
+										key={i + 1}>
+										<Skeleton height='420px' />
+										<Skeleton
+											height='25px'
+											mt='15px'
+										/>
+									</Flex>
+								);
+							})}
+						{data &&
+							data.mangas.map((item, i) => {
+								return isToggled ? (
+									<MangaList
+										data={item}
+										key={i + 1}
+									/>
+								) : (
+									<MangaTile
+										props={item}
+										key={i + 1}
+									/>
+								);
+							})}
+					</div>
 
-          <div
-            className={`${catalog.wrap} ${
-              isToggled ? catalog.grid_detail : catalog.grid_common
-            }`}
-          >
-            {!data &&
-              [...Array(24)].map((_, i) => {
-                return (
-                  <Flex flexDirection="column" key={i + 1}>
-                    <Skeleton height="420px" />
-                    <Skeleton height="25px" mt="15px" />
-                  </Flex>
-                );
-              })}
-            {data &&
-              data.mangas.map((item, i) => {
-                return isToggled ? (
-                  <MangaList data={item} key={i + 1} />
-                ) : (
-                  <MangaTile props={item} key={i + 1} />
-                );
-              })}
-          </div>
-
-          {data && (
-            <Pagination
-              router={router}
-              total={data.total}
-              offset={data.offset}
-              step={data.step}
-            />
-          )}
-        </div>
-      </div>
-    </>
-  );
+					{data && (
+						<Pagination
+							router={router}
+							total={data.total}
+							offset={data.offset}
+							step={data.step}
+						/>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
