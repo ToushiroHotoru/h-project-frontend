e41a770c152ad79a3c2d2f987cd33ed400704d8c
ToushiroHotoru <toushirohotoru@gmail.com>
2022-12-05 18:53:06 +0300

    Новый хостинг

--- libs/changeApiUrl.js
+++ libs/changeApiUrl.js
@@ -1,1 +1,4 @@
-export const LINK = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://h-project.herokuapp.com";
+export const LINK =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : "https://h-project.toushirohotoru.repl.co";
