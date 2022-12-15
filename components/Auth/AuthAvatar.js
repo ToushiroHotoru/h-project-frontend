59bc69a6c632fa0885ffed6c00f415dd87619b54
ToushiroHotoru <toushirohotoru@gmail.com>
2022-12-15 13:05:13 +0300

    тесе skeleton

--- components/Auth/AuthAvatar.js
+++ components/Auth/AuthAvatar.js
@@ -1,5 +1,6 @@
 import AuthFavoritesCSS from "../../styles/components/AuthFavorites.module.css";
 import Image from "next/image";
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 
 export default function AuthAvatar({
@@ -8,18 +9,20 @@ export default function AuthAvatar({
   setAvatarPreview,
 }) {
   const [isClicked, setIsClicked] = useState(false);
+  const [isLoaded, setIsloaded] = useState(false);
 
   useEffect(() => {
     // подсветка мини изобаржения снизу в зависимости от превью аватара
     setIsClicked(avatarImgUrl !== avatarPreview ? false : true);
   }, [avatarPreview, avatarImgUrl]);
   return (
-    <div
+    <SkeletonCircle
       className={
         isClicked
           ? AuthFavoritesCSS.avatar_image_clicked
           : AuthFavoritesCSS.avatar_image
       }
+      isLoaded={isLoaded}
       mx="1em"
       onClick={() => {
         // включение отключение рамки аватарки
@@ -29,12 +32,15 @@ export default function AuthAvatar({
       }}
     >
       <Image
+        onLoadingComplete={() => {
+          setIsloaded(true);
+        }}
         src={avatarImgUrl}
         width={70}
         height={70}
         alt="Picture of the author"
         draggable="false"
       />
-    </div>
+    </SkeletonCircle>
   );
 }
