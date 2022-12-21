cde37a8c9812c5b10b9334959c84923acb9a8c01
ToushiroHotoru <toushirohotoru@gmail.com>
2022-12-21 15:19:13 +0300

    Теги страница добавил

---
+++ pages/tags.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import TagDesktop from "../components/Tags/TagDesktop";
+import TagMobile from "../components/Tags/TagMobile";
+import TagsCss from "../styles/components/Tags.module.css";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
+
+export default function Tags() {
+  const [tags, setTags] = useState([
+    {
+      name: "tag1",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag1.png",
+    },
+    {
+      name: "tag2",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag2.png",
+    },
+    {
+      name: "tag3",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag3.png",
+    },
+    {
+      name: "tag4",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag4.png",
+    },
+    {
+      name: "tag5",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag1.png",
+    },
+    {
+      name: "tag6",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag2.png",
+    },
+    {
+      name: "tag7",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag3.png",
+    },
+    {
+      name: "tag8",
+      desc: "this is a description this is a description this is a description this is a description",
+      count: "423",
+      img: "/tag4.png",
+    },
+  ]);
+  return (
+    <div className={isMobile ? TagsCss.tagsDesktop : TagsCss.tagsMobile}>
+      {tags.map((item, i) => {
+        return isMobile ? (
+          <TagDesktop data={item} key={i + 1} />
+        ) : (
+          <TagMobile data={item} key={i + 1} />
+        );
+      })}
+    </div>
+  );
+}
