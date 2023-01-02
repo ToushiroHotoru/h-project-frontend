3b86ea138f815ecd50837be934ac77c4d5654912
ToushiroHotoru <toushirohotoru@gmail.com>
2023-01-02 16:24:52 +0300

    Выгрузка тегов

--- pages/tags/index.js
+++ pages/tags/index.js
@@ -1,68 +1,37 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Tag from "../../components/Tags/Tag";
 import TagsCss from "../../styles/components/Tags.module.css";
+import { LINK } from "../../libs/changeApiUrl.js";
 
 export default function Tags() {
-  const [tags, setTags] = useState([
-    {
-      name: "tag1",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 342,
-      img: "/tag1.png",
-    },
-    {
-      name: "tag2",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 23,
-      img: "/tag2.png",
-    },
-    {
-      name: "tag3",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 987,
-      img: "/tag3.png",
-    },
-    {
-      name: "tag4",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 323,
-      img: "/tag4.png",
-    },
-    {
-      name: "tag5",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 53,
-      img: "/tag1.png",
-    },
-    {
-      name: "tag6",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 765,
-      img: "/tag2.png",
-    },
-    {
-      name: "tag7",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 456,
-      img: "/tag3.png",
-    },
-    {
-      name: "tag8",
-      desc: "this is a description this is a description this is a description this is a description",
-      count: 235,
-      img: "/tag4.png",
-    },
-  ]);
+  const [tags, setTags] = useState([]);
+
+  const getTagsFunc = async () => {
+    try {
+      const res = await fetch(`${LINK}/get_tags`);
+      const result = await res.json();
+      setTags(result.tags);
+      console.log(result.tags);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTagsFunc();
+  }, []);
+
   return (
     <div className="container">
       <Head>
         <title>Теги</title>
       </Head>
       <div className={TagsCss.tagsDesktop}>
-        {tags.map((item, i) => {
-          return <Tag data={item} key={i + 1} />;
-        })}
+        {tags &&
+          tags.map((item, i) => {
+            return <Tag data={item} key={i + 1} />;
+          })}
       </div>
     </div>
   );
