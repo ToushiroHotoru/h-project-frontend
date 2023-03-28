d37e0d98ad2610c86117b3d4953ef1b93c983fe2
Kamiya_Nya <dikriss16@gmail.com>
2023-03-28 17:39:11 +0300

    Ебучая авторизация

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,21 +3,21 @@ import axios from "../../libs/axios";
 import { NextApiRequest, NextApiResponse } from "next/server";
 
 export default async (NextApiRequest, NextApiResponse) => {
-  const { headers, body } = NextApiRequest;
-
   try {
-    const { data, headers: returnedHeaders } = await axios.post(
-      "/login", // Node.js backend path
-      body, // Login body (email + password)
-      { headers } // Headers from the Next.js Client
-    );
-    //  Update headers on requester using headers from Node.js server response
+    const { headers, body } = NextApiRequest;
+    const res = NextApiResponse;
+    const response = await axios.post("/login", body, {
+      headers: headers,
+    });
+    const data = response.data;
+    const returnedHeaders = response.headers;
+
     Object.entries(returnedHeaders).forEach((keyArr) =>
-      res.setHeader(keyArr[0], keyArr[1].toString())
+      res.setHeader(keyArr[0], keyArr[1])
     );
-    res.send(data); // Send data from Node.js server response
-  } catch ({ response: { status, data } }) {
-    // Send status (probably 401) so the axios interceptor can run.
-    res.status(status).json(data);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    // NextApiResponse.status(status).json(data);
   }
 };
--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import axios from "../../libs/axios";
-import { LINK as API_URL } from "../../libs/changeApiUrl";
 
 export async function getServerSideProps(context) {
-  const res = await axios.post(`/profile`, {
-    username: context.params.username,
-  });
-  const data = res.data.message;
-  console.log(res.data.message);
+  const accessToken = context.req.cookies.accessToken;
+  console.log(context.req.cookies)
+  const res = await axios.post(
+    `/profile`,
+    {
+      username: context.params.username,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  const data = res.data.profile;
 
   return {
     props: {
