bd9de37f897985dace56e9ac8a38ef4bcf8aa9ff
Kamiya_Nya <dikriss16@gmail.com>
2023-03-30 17:37:41 +0300

    работа со стором - не окончательная

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, AnyAction } from "@reduxjs/toolkit";
+import { createWrapper, MakeStore, HYDRATE } from "next-redux-wrapper";
 import deviceTypeSlice from "./deviceTypeSlice";
 import selectedTagsSlice from "./selectedTagsSlice";
+import authSlice from "./slices/AuthSlice";
 
-export const store = configureStore({
-  reducer: {
+const combinedReducers = combineReducers({
     deviceType: deviceTypeSlice,
     selectedTagsSlice: selectedTagsSlice,
-  },
+    authReducer: authSlice,
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    return nextState;
+  }
+  return combinedReducers(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+const makeStore = () => store;
+
+export const wrapper = createWrapper(makeStore, { storeKey: "key" });
