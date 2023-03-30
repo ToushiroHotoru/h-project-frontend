4978fb7d382c85b9574c5e45a214ce34a7350ff7
Kamiya_Nya <dikriss16@gmail.com>
2023-03-30 17:38:32 +0300

    добавил асинхронные редюсеры для логина и логаута, изменил поля

--- redux/slices/AuthSlice.js
+++ redux/slices/AuthSlice.js
@@ -1,12 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosFront from "../../libs/axiosFront";
 
 const intenalInitialState = {
-  accessToken: "",
+  isAuth: false,
+  accessToken: null,
+  error: null,
   user: {
     id: null,
     userName: null,
   },
 };
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const loginResponse = await axiosFront.post("/api/login", {
+        email: email,
+        password: password,
+      });
+      return {
+        user: loginResponse.data.user,
+        accessToken: loginResponse.data.accessToken,
+      };
+    } catch (error) {
+      thunkAPI.rejectWithValue("Не удалось авторизоваться");
+    }
+  }
+);
+
+export const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+  try {
+    const loginResponse = await axiosFront.delete("/api/logout");
+    return loginResponse;
+  } catch (error) {
+    thunkAPI.rejectWithValue("Не удалось авторизоваться");
+  }
+});
 
 export const authSlice = createSlice({
   name: "auth",
@@ -17,6 +47,32 @@ export const authSlice = createSlice({
     },
     reset: () => intenalInitialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state, action) => {
+      state.isLoading = true;
+      state.isAuth = false;
+    });
+
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.error = "";
+      state.accessToken = action.payload.token;
+      state.user = action.payload.user;
+      state.isLoading = false;
+      state.isAuth = true;
+    });
+
+    builder.addCase(login.rejected, (state, action) => {
+      state.error = action.payload.error;
+      state.isLoading = false;
+      state.isAuth = false;
+    });
+
+    builder.addCase(logout.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(logout.fulfilled, (state, action) => intenalInitialState);
+  },
 });
-export const { updateAccessToken, reset } = authSlice.actions;
+
+export default authSlice.reducer;
