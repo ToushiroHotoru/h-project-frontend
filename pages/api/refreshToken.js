6e294e2b5fab5140cbe2e9b23555e3fa35321e49
Kamiya_Nya <dikriss16@gmail.com>
2023-03-31 17:25:58 +0300

    правки мелкие

--- pages/api/refreshToken.js
+++ pages/api/refreshToken.js
@@ -1,26 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "../../libs/axiosBack";
 import { NextApiRequest, NextApiResponse } from "next/server";
-import Cookies from "js-cookie";
 
 export default async (NextApiRequest, NextApiResponse) => {
+  const res = NextApiResponse;
   try {
-    console.log(NextApiRequest.cookies);
     const { headers } = NextApiRequest;
-    const res = NextApiResponse;
-    const response = await axios.get("/refresh");
 
-    // const data = response.data;
+    const response = await axios.get("/refresh", { headers });
+
+    const data = response.data;
     const returnedHeaders = response.headers;
 
-    // console.log(returnedHeaders);
     Object.entries(returnedHeaders).forEach((keyArr) =>
       res.setHeader(keyArr[0], keyArr[1])
     );
-    // res.send(data);
+    res.send(data);
   } catch (error) {
     // we don't want to send status 401 here.
-    // res.send(error);
-    // console.log(error);
+    res.send(error);
   }
 };
