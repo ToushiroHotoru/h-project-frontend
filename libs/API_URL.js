647cf5f497bc4432f149da9ab34be97e0dc48efe
Kamiya_Nya <dikriss16@gmail.com>
2023-03-31 17:25:40 +0300

    ссылки на сервер

---
+++ libs/API_URL.js
@@ -0,0 +1,7 @@
+// export const LINK = process.env.HOST
+const server = "repl";
+
+export const LINK =
+  server !== "repl"
+    ? "http://localhost:8080"
+    : "https://h-project.toushirohotoru.repl.co";
