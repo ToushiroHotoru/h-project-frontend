e98806351bb796de0dba42a62028c54516775a7c
KamiyaNya <dikriss16@gmail.com>
2023-04-24 18:09:52 +0300

    Комменты тянутся с другого места

--- components/Manga/MangaComments.js
+++ components/Manga/MangaComments.js
@@ -1,26 +1,6 @@
-import { useRouter } from "next/router";
 import MangaCommentsEmpty from "./MangaCommentsEmpty";
 import MangaCommentsExist from "./MangaCommentsExist";
-import { useState, useEffect } from "react";
-import axiosBack from "../../libs/axiosBack.js";
-export default function MangaComments() {
-  const router = useRouter();
-  const [comments, setComments] = useState([]);
-
-  const getComments = async () => {
-    const response = await axiosBack.get("/get_comments", {
-      params: {
-        mangaId: router.query.id,
-      },
-    });
-
-    setComments(response.data.comments);
-  };
-
-  useEffect(() => {
-    getComments();
-  }, []);
-  
+export default function MangaComments({ comments }) {
   if (comments.length) {
     return <MangaCommentsExist comments={comments} />;
   }
