0084228d7be4f9480065a437091ce678e52a5970
ToushiroHotoru <toushirohotoru@gmail.com>
2023-05-09 13:33:47 +0300

    Changed backend url

--- libs/API_URL.js
+++ libs/API_URL.js
@@ -4,4 +4,4 @@ const server = "repl";
 export const LINK =
   server !== "repl"
     ? "http://localhost:8080"
-    : "http://h-project.toushirohotoru.repl.co";
+    : "https://h-project-clf3.onrender.com";
