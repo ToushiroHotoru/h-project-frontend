326e234c8ea67ee953a749b91d677ec63db8d98d
KamiyaNya <dikriss16@gmail.com>
2023-12-15 15:59:28 +0300

    task-41

--- src/components/theme.js
+++ src/components/theme.js
@@ -9,7 +9,19 @@ const config = {
   useSystemColorMode: false,
 };
 
+const breakpoints = {
+  base: "0px",
+  x450: "450px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  x1170: "1170px",
+  xl: "1280px",
+  x1440: "1440px",
+  "2xl": "1536px",
+};
+
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, breakpoints: breakpoints });
 
 export default theme;
