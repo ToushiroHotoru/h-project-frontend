ff547108612f8c65413f01f41a1f3bbe9f513890
ITz ME ДАНЯ <dikriss16@gmail.com>
2024-02-20 19:04:24 +0300

    Работа над профилем

--- src/pages/user/[username].js
+++ src/pages/user/[username].js
@@ -1,49 +1,168 @@
 import Head from "next/head";
+import Image from "next/image";
+import { Box, Flex, Skeleton, Heading } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 import axios from "@/utils/axios";
+import ErrorDefault from "./../../components/errors/errorDefault/errorDefault";
 
-export async function getServerSideProps(context) {
-  const username = context.query.username;
-  const res = await axios.get(`/api/user`, {
-    params: {
-      username: username,
-    },
-  });
-  const user = res.data.user;
-  return {
-    props: { user },
+export default function Profile() {
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState({ code: null, message: "" });
+  const router = useRouter();
+
+  const getCurrentUser = async () => {
+    setIsLoading(true);
+    const username = router.query.username;
+    axios
+      .get(`/user`, {
+        params: {
+          username: username,
+        },
+      })
+      .then((response) => {
+        const { data, status, message } = response.data;
+        if (status === "success") {
+          setIsError(false);
+          setError({ code: null, message: "" });
+          setUser(data.user);
+          setIsLoading(false);
+
+          console.log(user.username.split(""));
+        }
+      })
+      .catch((response) => {
+        setIsError(true);
+        setError({ code: response.status, message: response.message });
+        setIsLoading(false);
+      });
   };
-}
 
-export default function Profile({ user }) {
-  if (!user) {
+  useEffect(() => {
+    getCurrentUser();
+  }, [router.query.username]);
+
+  if (!user && isLoading) {
     return (
       <>
         <Head>
           <title>Профиль</title>
         </Head>
 
-        <div className="profile">
+        <Flex height="100%" position="relative">
           <div className="container">
-            <h1>Не авторизованы</h1>
+            <Box>
+              <Box
+                borderTopLeftRadius="10px"
+                borderTopRightRadius="10px"
+                width="100%"
+                height="160px"
+                backgroundColor="var(--pink)"
+              >
+                <Box textAlign="center" fontSize="52px" pt="24px">
+                  H-PROJECT
+                </Box>
+                <Skeleton isLoaded={isLoading}>
+                  <Flex justifyContent="center">
+                    <Image
+                      src="/zero.png"
+                      width="100px"
+                      height="100px"
+                      objectFit="cover"
+                    />
+                  </Flex>
+                </Skeleton>
+              </Box>
+              <Skeleton isLoaded={isLoading}>
+                <Heading as="h1">User</Heading>
+              </Skeleton>
+            </Box>
           </div>
-        </div>
+        </Flex>
       </>
     );
   }
 
-  if (user) {
+  if (!isLoading && isError) {
     return (
       <>
         <Head>
           <title>Профиль</title>
         </Head>
 
-        <div className="profile">
+        <Flex height="100%" position="relative">
           <div className="container">
-            <h1>Профиль</h1>
-            <div>{user.username}</div>
+            <ErrorDefault code={error.code} message={error.message} />
           </div>
-        </div>
+        </Flex>
+      </>
+    );
+  }
+
+  if (!isLoading && user) {
+    return (
+      <>
+        <Head>
+          <title>Профиль</title>
+        </Head>
+
+        <Flex height="100%" position="relative">
+          <div className="container">
+            <Box>
+              <Box
+                borderTopLeftRadius="10px"
+                borderTopRightRadius="10px"
+                width="100%"
+                height="160px"
+                backgroundColor="var(--pink)"
+              >
+                <Box textAlign="center" fontSize="52px" pt="24px">
+                  H-PROJECT
+                </Box>
+
+                {/* <Skeleton isLoaded={isLoading}> */}
+                <Flex
+                  justifyContent="center"
+                  borderRadius="50%"
+                  overflow="hidden"
+                  width="100px"
+                  height="100px"
+                  mx="auto"
+                >
+                  {user.avatar?.image ? (
+                    <Image
+                      src={user.avatar?.image}
+                      width="100px"
+                      height="100px"
+                      objectFit="cover"
+                    />
+                  ) : (
+                    <Flex
+                      width="100%"
+                      height="100%"
+                      backgroundColor="grey"
+                      justifyContent="center"
+                      alignItems="center"
+                      fontSize="44px"
+                    >
+                      {user.username?.split("")[0].toUpperCase()}
+                    </Flex>
+                  )}
+                </Flex>
+                {/* </Skeleton> */}
+              </Box>
+              <Heading as="h1" textAlign="center" mt="54px">
+                {user.username}
+              </Heading>
+              <Box textAlign="center" fontSize="14px">
+                {user.role?.roleRu}
+              </Box>
+            </Box>
+          </div>
+        </Flex>
       </>
     );
   }
