99ab5c2495522ad31e282c1cd0b6dd76a0fb669c
ITz ME ДАНЯ <dikriss16@gmail.com>
2024-02-21 13:00:10 +0300

    перенес на serverside rendering

--- src/pages/user/[username].js
+++ src/pages/user/[username].js
@@ -1,108 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Box, Flex, Skeleton, Heading } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 import axios from "@/utils/axios";
-import ErrorDefault from "./../../components/errors/errorDefault/errorDefault";
 
-export default function Profile() {
-  const [user, setUser] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState({ code: null, message: "" });
-  const router = useRouter();
-
-  const getCurrentUser = async () => {
-    setIsLoading(true);
-    const username = router.query.username;
-    axios
-      .get(`/user`, {
-        params: {
-          username: username,
-        },
-      })
-      .then((response) => {
-        const { data, status, message } = response.data;
-        if (status === "success") {
-          setIsError(false);
-          setError({ code: null, message: "" });
-          setUser(data.user);
-          setIsLoading(false);
-
-          console.log(user.username.split(""));
-        }
-      })
-      .catch((response) => {
-        setIsError(true);
-        setError({ code: response.status, message: response.message });
-        setIsLoading(false);
-      });
+export async function getServerSideProps({ query }) {
+  const response = await axios.get(`/user`, {
+    params: {
+      username: query.username,
+    },
+  });
+  const { data, status, message } = response?.data;
+  const statusCode = response.status;
+  if (status !== "success") {
+    return {
+      notFound: true,
+      props: {
+        code: statusCode,
+        message: message,
+      },
+    };
+  }
+  return {
+    props: {
+      user: data.user,
+    },
   };
+}
 
-  useEffect(() => {
-    getCurrentUser();
-  }, [router.query.username]);
-
-  if (!user && isLoading) {
-    return (
-      <>
-        <Head>
-          <title>Профиль</title>
-        </Head>
-
-        <Flex height="100%" position="relative">
-          <div className="container">
-            <Box>
-              <Box
-                borderTopLeftRadius="10px"
-                borderTopRightRadius="10px"
-                width="100%"
-                height="160px"
-                backgroundColor="var(--pink)"
-              >
-                <Box textAlign="center" fontSize="52px" pt="24px">
-                  H-PROJECT
-                </Box>
-                <Skeleton isLoaded={isLoading}>
-                  <Flex justifyContent="center">
-                    <Image
-                      src="/zero.png"
-                      width="100px"
-                      height="100px"
-                      objectFit="cover"
-                    />
-                  </Flex>
-                </Skeleton>
-              </Box>
-              <Skeleton isLoaded={isLoading}>
-                <Heading as="h1">User</Heading>
-              </Skeleton>
-            </Box>
-          </div>
-        </Flex>
-      </>
-    );
-  }
-
-  if (!isLoading && isError) {
-    return (
-      <>
-        <Head>
-          <title>Профиль</title>
-        </Head>
-
-        <Flex height="100%" position="relative">
-          <div className="container">
-            <ErrorDefault code={error.code} message={error.message} />
-          </div>
-        </Flex>
-      </>
-    );
-  }
-
-  if (!isLoading && user) {
+export default function Profile({ user }) {
+  if (user) {
     return (
       <>
         <Head>
